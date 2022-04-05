938a4116629c6b24ca2532d029ceff3436b520e7
Joel Lake <joelcooperlake@gmail.com>
2022-04-04 20:16:55 -0700

    adding this pages

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const beverage = $("#beverage").val();
+
+const flavor = $("input:radio[name=flavor]:checked").val();
+
+const dob = $("#born").val();
+
+const favoriteColor = $("#color").val();
+
+
+//not code
+
+const number2 = parseInt($("#add2").val());
